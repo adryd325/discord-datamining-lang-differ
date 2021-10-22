d2d6a63a052c6ae15f80018b9e2bbd6c51c93873
Tiemen <ThaTiemsz@users.noreply.github.com>
2021-10-22 08:58:54 +0200

    Fix for Webpack 5 update

--- getLangStrings.js
+++ getLangStrings.js
@@ -5,46 +5,37 @@ module.exports = getLangStrings
 
 // I really really hate this, but this is much safer than regex+eval
 function getLangStrings(file) {
-  const webpackModules = esprima.parse(file).body[0].expression.arguments[0]
-    .elements[1].elements;
+  const webpackModules = esprima.parse(file).body[0].expression.arguments[0].elements;
 
   const allStrings = {};
-  // lang file
 
-  webpackModules.forEach((webpackModule) => {
-    if (
-      webpackModule &&
-      webpackModule.body.body[1] &&
-      webpackModule.body.body[1].expression &&
-      webpackModule.body.body[1].expression.right &&
-      webpackModule.body.body[1].expression.right.callee &&
-      webpackModule.body.body[1].expression.right.callee.object &&
-      webpackModule.body.body[1].expression.right.callee.object.name ===
-        "Object" &&
-      webpackModule.body.body[1].expression.right.callee.property.name ===
-        "freeze" &&
-      webpackModule.body.body[1].expression.right.arguments[0]
-    ) {
+  webpackModules.forEach((webpackChunk) => {
+    webpackChunk?.properties?.forEach((webpackModule) => {
       if (
-        webpackModule.body.body[1].expression.right.arguments[0].properties &&
-        webpackModule.body.body[1].expression.right.arguments[0].properties.some(
-          (suspectedLangModule) => {
-            if (
-              suspectedLangModule.key.name === "DISCORD_NAME" ||
-              suspectedLangModule.key.name === "TEAL"
-            ) {
-              return true;
-            }
-          }
-        )
+        webpackModule?.value?.body?.body?.[1]?.expression?.right?.callee?.object?.name === "Object" &&
+        webpackModule?.value?.body?.body?.[1]?.expression?.right?.callee?.property?.name === "freeze" &&
+        webpackModule?.value?.body?.body?.[1]?.expression?.right?.arguments[0]
       ) {
-        webpackModule.body.body[1].expression.right.arguments[0].properties.forEach(
-          (langEntry) => {
-            allStrings[langEntry.key.name] = langEntry.value.raw;
-          }
-        );
+        if (
+          webpackModule.value.body.body[1].expression.right.arguments[0].properties.some(
+            (suspectedLangModule) => {
+              if (
+                suspectedLangModule.key.name === "DISCORD_NAME" ||
+                suspectedLangModule.key.name === "TEAL"
+              ) {
+                return true;
+              }
+            }
+          )
+        ) {
+          webpackModule.value.body.body[1].expression.right.arguments[0].properties.forEach(
+            (langEntry) => {
+              allStrings[langEntry.key.name] = langEntry.value.raw;
+            }
+          );
+        }
       }
-    }
+    });
   });
   return allStrings
 }
