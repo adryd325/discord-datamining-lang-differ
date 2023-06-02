5eb578311d1decf44fad3a58e17ceb357c9bbca5
xHyroM <generalkubo@gmail.com>
2023-06-02 14:41:07 +0200

    docs: remove comment

--- tests/testGetLangStrings.js
+++ tests/testGetLangStrings.js
@@ -1,7 +1,6 @@
 import getLangStrings from "../getLangStrings.js";
 import { readFileSync, writeFileSync } from "fs";
 const c = getLangStrings(readFileSync(process.argv[2]));
-//console.log(c);
 
 writeFileSync(
   "./test-files/test-lang-strings.json",
