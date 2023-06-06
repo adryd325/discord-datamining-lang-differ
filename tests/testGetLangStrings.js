be05128f5e4fc497c4538b85e39b553e14d71a4d
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2023-06-06 18:00:17 +0200

    tests: allow to pass file name to args

--- tests/testGetLangStrings.js
+++ tests/testGetLangStrings.js
@@ -3,6 +3,6 @@ import { readFileSync, writeFileSync } from "fs";
 const c = getLangStrings(readFileSync(process.argv[2]));
 
 writeFileSync(
-  "./test-files/test-lang-strings.json",
+  process.argv[3],
   JSON.stringify(c, null, 2)
 );
