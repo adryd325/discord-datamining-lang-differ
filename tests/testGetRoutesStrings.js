fa0a30dc87278db6ec81a0b94f1a552ab5cdb8d5
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2023-06-06 18:00:01 +0200

    Update testGetRoutesStrings.js

--- tests/testGetRoutesStrings.js
+++ tests/testGetRoutesStrings.js
@@ -2,4 +2,4 @@ import getRoutesStrings from "../getRoutesStrings.js";
 import { readFileSync, writeFileSync } from "fs";
 const c = getRoutesStrings(readFileSync(process.argv[2]));
 
-writeFileSync("./test-files/routes.json", JSON.stringify(c, null, 2));
+writeFileSync(process.argv[3], JSON.stringify(c, null, 2));
