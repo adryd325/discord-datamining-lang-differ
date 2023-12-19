4ae456c59c26ee7a8e254e1bdc059da204ed3a90
Amia <9750071+aamiaa@users.noreply.github.com>
2023-12-19 19:22:44 +0100

    rewrite getLangStrings properly

--- getLangStrings.js
+++ getLangStrings.js
@@ -1,87 +1,19 @@
-// HOLY SHIT I HATE THIS
+import * as acorn from "acorn";
+import * as walk from "acorn-walk"
 
-import { parse } from "espree";
-export default getLangStrings;
+export default function getLangStrings(file) {
+	const allStrings = {};
 
-// I really really hate this, but this is much safer than regex+eval
-function getLangStrings(file) {
-  const tree = parse(file, {
-    ecmaVersion: 2022,
-  });
+	const ast = acorn.parse(file, {ecmaVersion: "2022"});
+	walk.simple(ast, {
+		ObjectExpression(node) {
+			if(node.properties.find(x => x.key?.type === "Identifier" && (x.key.name === "DISCORD_NAME" || x.key.name === "DISCORD_DESC_SHORT"))) {
+				for(const property of node.properties) {
+					allStrings[property.key.name] = property.value.raw;
+				}
+			}
+		}
+	})
 
-  const webpackModules =
-    tree.body[0].expression.arguments[0].elements[1].properties;
-
-  const allStrings = {};
-
-  function parseStrings(webpackModule) {
-    const expression = webpackModule?.value?.body?.body?.[2]?.expression;
-    if (!expression) {
-      return;
-    }
-
-    if (
-      expression.right?.callee?.object?.name === "Object" &&
-      expression.right?.callee?.property?.name === "freeze" &&
-      expression.right?.arguments?.[0].expressions?.[0]?.arguments?.[0]
-    ) {
-      // parse frozen object
-      const properties =
-        expression.right.arguments[0].expressions[0].arguments[0].right
-          .properties;
-      if (
-        properties.some((suspectedLangModule) => {
-          if (
-            suspectedLangModule.key.name === "DISCORD_DESC_SHORT" ||
-            suspectedLangModule.key.name === "DISCORD_NAME"
-          ) {
-            return true;
-          }
-        })
-      ) {
-        properties.forEach((langEntry) => {
-          allStrings[langEntry.key.name] = langEntry.value.raw;
-        });
-      }
-
-      // parse function call arguments
-      expression.right.arguments[0].expressions.forEach((callExpr) => {
-        if (callExpr.arguments?.[1] && callExpr.arguments?.[2])
-          allStrings[callExpr.arguments[1].value] = callExpr.arguments[2].raw;
-      });
-    }
-  }
-
-  function parseUntranslatedStrings(webpackModule) {
-    const expression = webpackModule?.value?.body?.body?.[0]?.expression;
-    if (!expression) {
-      return;
-    }
-
-    if (
-      expression.right?.callee?.object?.name === "Object" &&
-      expression.right?.callee?.property?.name === "freeze" &&
-      expression.right?.arguments[0]?.properties
-    ) {
-      const properties = expression.right.arguments[0].properties;
-      if (
-        properties.every(
-          (suspectedLangModule) =>
-            suspectedLangModule.key.type === "Identifier" &&
-            suspectedLangModule.value.type === "Literal"
-        )
-      ) {
-        properties.forEach((langEntry) => {
-          allStrings[langEntry.key.name] = langEntry.value.raw;
-        });
-      }
-    }
-  }
-
-  webpackModules.forEach((webpackModule) => {
-    parseStrings(webpackModule);
-    parseUntranslatedStrings(webpackModule);
-  });
-
-  return allStrings;
+	return allStrings;
 }
